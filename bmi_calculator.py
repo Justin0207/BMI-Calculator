d3add77dce1007f2af36234a73d9f97137ca26d8
Anyanwu_Justice <105282903+Justin0207@users.noreply.github.com>
2024-03-15 14:47:50 +0100

    Update bmi_calculator.py

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -97,16 +97,16 @@ if st.button('Calculate BMI : '):
 
 
 st.markdown("Made with ❤️ by [Anyanwu Justice](https://github.com/Justin0207)")
-    st.write(
-    """
-    <style>
-        footer {
-            visibility: hidden;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
+st.write(
+"""
+<style>
+footer {
+    visibility: hidden;
+}
+</style>
+""",
+unsafe_allow_html=True
+)
 
 
 
